392c8998903d716255fec5b893c78fef4298e1df
Fabian Keunecke <fkeunecke@frobese.de>
2016-05-20 15:24:00 +0200

    Date parsing, Add date parser tests

--- lib/ex_ical/date_parser.ex
+++ lib/ex_ical/date_parser.ex
@@ -3,14 +3,16 @@ defmodule ExIcal.DateParser do
 
   def parse(data), do: parse(data, nil)
 
+  def parse(data, timezone) when is_nil(timezone), do: parse(data, :utc)
+
   def parse(<< year :: binary-size(4), month :: binary-size(2), day :: binary-size(2), "T",
-                        hour :: binary-size(2), minutes :: binary-size(2), seconds :: binary-size(2), "Z" >>, nil) do
+                        hour :: binary-size(2), minutes :: binary-size(2), seconds :: binary-size(2), "Z" >>, timezone) do
       %DateTime{year: year |> String.to_integer, month: month |> String.to_integer, day: day |> String.to_integer,
-                hour: hour |> String.to_integer, minute: minutes |> String.to_integer, second: seconds |> String.to_integer, timezone: Timezone.get(:utc)}
+                hour: hour |> String.to_integer, minute: minutes |> String.to_integer, second: seconds |> String.to_integer, timezone: Timezone.get(timezone)}
   end
 
-  def parse(<< year :: binary-size(4), month :: binary-size(2), day :: binary-size(2), "Z" >>, nil) do
-      %DateTime{year: year |> String.to_integer, month: month |> String.to_integer, day: day |> String.to_integer, timezone: Timezone.get(:utc)}
+  def parse(<< year :: binary-size(4), month :: binary-size(2), day :: binary-size(2), "Z" >>, timezone) do
+      %DateTime{year: year |> String.to_integer, month: month |> String.to_integer, day: day |> String.to_integer, timezone: Timezone.get(timezone)}
   end
 
   def parse(<< year :: binary-size(4), month :: binary-size(2), day :: binary-size(2), "T",
